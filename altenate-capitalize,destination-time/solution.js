5400391798f63970de58dec0aff109ec82f3630c
ria agnes jose <joseagnesria@gmail.com>
2018-05-24 23:04:48 -0700

    changed the solution of one code challenge

--- altenate-capitalize,destination-time/solution.js
+++ altenate-capitalize,destination-time/solution.js
@@ -1,32 +1,16 @@
-function capitalize(s){
-  var answer =[];
-  var answer2 =[];
-  var final =[]
-  var split = s.split('');
-  for(var i = 0; i<split.length;i++){
- if(i%2 === 0){
-  var target = split[i].toUpperCase();
-  answer.push(target)
- }
- else{
-   answer.push(split[i]);
- }
-  }
- 
-  
-  for(var j =0;j<split.length;j++){
-   
-    if(j%2 === 0){
-      
-  answer2.push(split[j])
+function toCamelCase(str){
+  var array = str.split('');
+  var separate =[];
+  var iphem =[];
+  for(var i = 0;i<array.length; i++){
+    if(array[i]=== '-'||array[i]==='_'){
+      iphem.push(array[i])
+      separate.push(array[i+1].toUpperCase());
+      i++;
     }
-     else{
-       var target2 =split[j].toUpperCase()
-   answer2.push(target2);
- }
+    else{
+      separate.push(array[i]);
+    }
   }
-var joinanswer = answer.join('');
-var joinanswer2 = answer2.join('');
-final.push(joinanswer,joinanswer2);
-return final
-}
+  return separate.join('');
+  }
--- replace-all-dots/solution.js
+++ replace-all-dots/solution.js
@@ -2,7 +2,7 @@ var replaceDots = function(str) {
   var strArray = str.split('');
   var array =[];
   for(var i = 0; i<strArray.length; i++){
-    console.log('j')
+  
     if(strArray[i] === '.'){
    array.push('-');
     }
